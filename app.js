2f944ee03dc7d0d163c7864e687abd731dc34595
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-04-19 09:04:21 +0100

    refactored the code

--- app.js
+++ app.js
@@ -1,77 +1,67 @@
 const data = [
-   {
-     principal: 2500,
-     time: 1.8
-   },
-   {
-     principal: 1000,
-     time: 5
-   },
-   {
-     principal: 3000,
-     time: 1
-   },
-   {
-     principal: 2000,
-     time: 3
-   }
- ];
+  {
+    principal: 2500,
+    time: 1.8
+  },
+  {
+    principal: 1000,
+    time: 5
+  },
+  {
+    principal: 3000,
+    time: 1
+  },
+  {
+    principal: 2000,
+    time: 3
+  }
+];
 
- const interestCalculator = (arr) => {
-   let rate;
-   let interest;
-   const interestData = [];
-   arr.forEach(data => {
-      if (data.principal >= 2500 && data.time > 1 && data.time < 3) {
-        rate = 3;
-        interest = (data.principal * rate * data.time) / 100;
-        interestData.push(
-          {
-            'principal':  data.principal,
-            'rate': rate,
-            'time': data.time,
-            'interest': interest
-          }
-        );
-      } else if (data.principal >= 2500 && data.time >= 3){
-       rate = 4;
-       interest = (data.principal * rate * data.time) / 100;
-       interestData.push(
-          {
-            'principal':  data.principal,
-            'rate': rate,
-            'time': data.time,
-            'interest': interest
-          }
-        );
-      } else if (data.principal < 2500 || data.time <= 1) {
-       rate = 2; 
-       interest = (data.principal * rate * data.time) / 100;
-       interestData.push(
-          {
-            'principal':  data.principal,
-            'rate': rate,
-            'time': data.time,
-            'interest': interest
-          }
-        );
-      } else {
-       rate = 1;
-       interest = (data.principal * rate * data.time) / 100;
-       interestData.push(
-          {
-            'principal':  data.principal,
-            'rate': rate,
-            'time': data.time,
-            'interest': interest
-          }
-        );
-      }
-   })
-   
-   console.log(interestData)
-  
-  return interestData;
+const finalInterest = (principal, rate, time, interest) => {
+ return {
+     principal,
+     rate,
+     time,
+     interest
  }
+}
+
+
+const interestCalculator = (arr) => {
+  let rate;
+  let interest;
+  const interestData = [];
+  arr.forEach(data => {
+     if (data.principal >= 2500 && data.time > 1 && data.time < 3) {
+       rate = 3;
+       interest = (data.principal * rate * data.time) / 100;
+       interestData.push(
+       finalInterest(data.principal, rate, data.time, interest)
+       );
+     } else if (data.principal >= 2500 && data.time >= 3){
+      rate = 4;
+      interest = (data.principal * rate * data.time) / 100;
+      interestData.push(
+       finalInterest(data.principal, rate, data.time, interest)
+       );
+     } else if (data.principal < 2500 || data.time <= 1) {
+      rate = 2; 
+      interest = (data.principal * rate * data.time) / 100;
+      interestData.push(
+      finalInterest(data.principal, rate, data.time, interest)
+       );
+     } else {
+      rate = 1;
+      interest = (data.principal * rate * data.time) / 100;
+      interestData.push(
+       finalInterest(data.principal, rate, data.time, interest)
+       );
+     }
+  })
+  
+  console.log(interestData)
  
- interestCalculator(data)
+ return interestData;
+}
+
+interestCalculator(data)
